30690a65d4c05d8bf447b87e58a7e3c2a5e12f37
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-07-05 13:04:46 +0200

    chore(cleanup): added comments
    cleaned up some code
    added comments
    a bit better UI

--- src/app/services/on-demand-load.service.ts
+++ src/app/services/on-demand-load.service.ts
@@ -7,6 +7,12 @@ import { Injectable } from '@angular/core';
 export class OnDemandLoadService {
   subject = new Subject<string>();
 
+  /**
+   * The function updates the value of the `subject` and causes the OnDemandPreloadStrategy to
+   * check all routes against this new value.
+   * @param moduleName - the 'name' property defined for each route as
+   * {data: { name: MODULE_NAME }}
+   */
   startPreload(moduleName: string) {
     this.subject.next(moduleName);
   }
--- src/app/services/on-demand-preloading-strategy.service.ts
+++ src/app/services/on-demand-preloading-strategy.service.ts
@@ -14,10 +14,22 @@ export class OnDemandPreloadStrategy implements PreloadingStrategy {
     this.preloadOnDemand$ = this.preloadOnDemandService.subject;
   }
 
+  /**
+   * The function that listens to the preloadOnDemand subject.
+   * Whenever a new value is provided to the subject, this function
+   * checks its value against EACH ROUTE. And if any route matches the 'name'
+   * property with the value provided to the subject, the route's module is loaded.
+   * @param route - the route being checked
+   * @param load - method from Angular to load module
+   * @returns Observable
+   */
   preload(route: Route, load: () => Observable<any>): Observable<any> {
     return this.preloadOnDemand$.pipe(
       mergeMap((moduleName) => {
         const shouldPreload = route.data?.['name'] === moduleName;
+        if (shouldPreload) {
+          console.log('Attempted to load module: ', route.data?.['name']);
+        }
         return shouldPreload ? load() : of(null);
       })
     );
